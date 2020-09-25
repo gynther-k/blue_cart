5a61381606c712fe02181ceaaee205b8071435b3
gynther-k <gunnar.applelid@gmail.com>
2020-09-25 16:04:41 +0200

    Remove old files

--- product-services/product_review_service/src/Controller.py
+++
@@ -1,6 +0,0 @@
-def retrieve_review_By_Product_Id(db,pid):
-    search = {"product_id" : pid}
-    collection = db.reviews 
-    result = list(collection.find(search)) 
-
-    return result
--- product-services/product_shopping_cart_service/src/Controller.py
+++
@@ -1,6 +0,0 @@
-def retrieve_shopping_cart_by_customer_id(db,cid):
-    search = {"customer_Id":cid}
-    collection = db.shopping_cart 
-    result = collection.find_one(search) 
-
-    return result
