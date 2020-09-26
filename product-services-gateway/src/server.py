f36c532b8fd101531e4e83974333795738e4b99c
Javier Ron <javierron90@gmail.com>
2020-09-26 16:57:12 +0200

    fixes on rabbit mq code 2
    Signed-off-by: Javier Ron <javierron90@gmail.com>

--- product-services-gateway/src/server.py
+++ product-services-gateway/src/server.py
@@ -16,30 +16,19 @@ class JSONEncoder(json.JSONEncoder):
 
 @app.route('/product_service_proxy', methods=['GET'])
 def post_route():
-    print("Hello? Anyone there?", flush=True)
-    print("Hello? Anyone there?", flush=True)
-    print("Hello? Anyone there?", flush=True)
-    print("Hello? Anyone there?", flush=True)
-
-    print("RECEIVE GET")
     start_time = time.time()
     product_id=request.headers["Product-Id"]
     customer_id=request.headers["Customer-Id"]
-    print("prduct_id and customer id", flush=True)
-    print(product_id, flush=True)
-    print(customer_id, flush=True)
 
     result=Controller.doTheMagic(product_id,customer_id)
   
-    # if result:
-    #     result=json.dumps({"result":json.loads(result),"Duration":((time.time() - start_time)*1000)}) 
-    #     result=json.loads(result)
-    #     result=JSONEncoder().encode(result)
-    # else:
-    #     print("fail")
-    #     return jsonify(error=str({"message": "not found!"})), 404
-
-    print(result)
+    if result:
+        result = json.dumps({"result":json.loads(result),"duration":((time.time() - start_time)*1000)}) 
+        result = json.loads(result)
+        result = JSONEncoder().encode(result)
+    else:
+        print("fail")
+        return jsonify(error=str({"message": "not found!"})), 404
 
     return make_response(result,200)
 
