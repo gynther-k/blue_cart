6db5493ebb4fbccbe82918e1029eb80c97dac913
gynther-k <gunnar.applelid@gmail.com>
2020-09-27 17:53:51 +0200

    remove old files

--- product-services/product_recommendation_service/src/Controller.py
+++
@@ -1,6 +0,0 @@
-def retrieve_recommendation_by_product_id(db,pid):
-    search = {"product_Id":pid}
-    collection = db.recommendation 
-    result = collection.find_one(search) 
-
-    return result
